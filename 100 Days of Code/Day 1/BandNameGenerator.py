1e0d6855eb3272b47dc03a1e76db7831a0c89245
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-12-23 00:38:11 +0530

    Create BandNameGenerator.py

---
+++ 100 Days of Code/Day 1/BandNameGenerator.py
@@ -0,0 +1,5 @@
+# This is the Day 0 of 100 Days of Code Challenge. It is for band name generator.
+print("******* Welcome to the Band Name Generator")
+city = input("What is the name of the city you grew up in?\n")
+pet = input("What's your pet's name\n")
+print(f"The name of the band in your city is {city}{pet}")
